3ed91d0553b60f5f3f855309094633f92aef7967
Unknown <hammarstrand@hotmail.com>
2019-03-06 20:44:27 +0100

    Added generic read input method.

--- Lesson2ModelleringEntity/ReadInput.cs
+++ Lesson2ModelleringEntity/ReadInput.cs
@@ -35,7 +35,6 @@ namespace Lesson2ModelleringEntity
                         Console.Write("Please enter a valid integer: ");
                     }
                 }
-
                 return (T)Convert.ChangeType(number, typeof(T));
             }
             else if (typeof(T) == typeof(DateTime))
@@ -53,7 +52,6 @@ namespace Lesson2ModelleringEntity
                         Console.Write("Please enter a valid date: ");
                     }
                 }
-
                 return (T)Convert.ChangeType(date, typeof(T));
             }
             else if (typeof(T) == typeof(bool))
@@ -75,80 +73,9 @@ namespace Lesson2ModelleringEntity
                         Console.Write("Please enter either Y or N: ");
                     }
                 }
-
                 return (T)Convert.ChangeType(value, typeof(T));
             }
             else throw new InvalidDataException();
-        }
-
-        public static string ReadString(string prompt)
-        {
-            Console.Write(prompt + ": ");
-            return Console.ReadLine();
-        }
-
-        public static int ReadInt(string prompt)
-        {
-            Console.Write(prompt + ": ");
-            int? number = null;
-            while (number == null)
-            {
-                string input = Console.ReadLine();
-                try
-                {
-                    number = int.Parse(input);
-                }
-                catch
-                {
-                    Console.Write("Please enter a valid integer: ");
-                }
-            }
-
-            return (int)number;
-        }
-
-        public static DateTime ReadDate(string prompt)
-        {
-            Console.Write(prompt + ": ");
-            DateTime? date = null;
-            while (date == null)
-            {
-                string input = Console.ReadLine();
-                try
-                {
-                    date = DateTime.Parse(input);
-                }
-                catch
-                {
-                    Console.Write("Please enter a valid date: ");
-                }
-            }
-
-            return (DateTime)date;
-        }
-
-        public static bool ReadBool(string prompt)
-        {
-            Console.Write(prompt + ": ");
-            bool? value = null;
-            while (value == null)
-            {
-                string input = Console.ReadLine();
-                if (input.ToUpper() == "Y")
-                {
-                    value = true;
-                }
-                else if (input.ToUpper() == "N")
-                {
-                    value = false;
-                }
-                else
-                {
-                    Console.Write("Please enter either Y or N: ");
-                }
-            }
-
-            return (bool)value;
         }
     }
 }
